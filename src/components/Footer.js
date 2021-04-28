f7304e1e2e44368368da6b86e012479e28b32734
Mohammad Altamimi <m98altamimi@gmail.com>
2021-04-28 16:14:07 +0300

    lab 1

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class Footer extends React.Component{
+render(){
+    return (
+        <footer>
+
+        <h4>&copy; | By Mohammad Altamimi </h4>
+        
+        </footer>
+
+    )
+}
+}
+
+
+ export default Footer;
